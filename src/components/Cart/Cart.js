8c492f154f34f9effecb1fa4c21439460e871f40
abir246 <abir246n@gmail.com>
2020-04-16 23:29:00 +0600

    Ema-john done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
     //const total = cart.reduce((total, prd) => total + prd.price,0);
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
@@ -27,10 +28,14 @@ const Cart = (props) => {
         <div>
             <h4>Order summary</h4>
             <p>Items Ordered: {cart.length}</p>
-            <p>Product Price: {total}</p>
+            <p>Product Price: {grandTotal}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + Vat: {tax}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br/>
+            {
+                props.children
+            }
         </div>
     );
 };
